776df2ab9be5deb31b0b69fe3a13b191871d3737
Sebastian Smiley <ssmiley@autoidm.com>
2024-06-20 12:26:13 -0400

    linting

--- tap_bamboohr/tap.py
+++ tap_bamboohr/tap.py
@@ -20,7 +20,7 @@ from tap_bamboohr.streams import (
 
 PLUGIN_NAME = "tap-bamboohr"
 
-STREAM_TYPES = [ #CustomReport has special handing below
+STREAM_TYPES = [  # CustomReport has special handing below
     Employees,
     EmploymentHistoryStatus,
     JobInfo,
@@ -59,7 +59,7 @@ class TapBambooHR(Tap):
             description=(
                 "Either `fail` or `ignore`. Determines behavior when fields returned "
                 "by API don't match fields specified in tap config.",
-            )
+            ),
         ),
         th.Property(
             "custom_reports",
@@ -85,17 +85,19 @@ class TapBambooHR(Tap):
             description=(
                 "CustomReport full body definition, example in meltano.yml, same "
                 "format as the Body for the POST request [here](https://documentation.bamboohr.com/reference/request-custom-report-1)"
-            )
+            ),
         ),
     ).to_dict()
 
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
-        streams =  [stream_class(tap=self) for stream_class in STREAM_TYPES]
-        custom_reports = self.config.get("custom_reports") 
-        if (custom_reports):
+        streams = [stream_class(tap=self) for stream_class in STREAM_TYPES]
+        custom_reports = self.config.get("custom_reports")
+        if custom_reports:
             for report in self.config.get("custom_reports"):
-                custom_report = CustomReport(tap=self, name=report["name"], custom_report_config=report)
+                custom_report = CustomReport(
+                    tap=self, name=report["name"], custom_report_config=report
+                )
                 streams.append(custom_report)
         return streams
 
